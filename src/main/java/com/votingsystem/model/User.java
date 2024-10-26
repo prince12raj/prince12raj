a1936afc1be30b3ce725443acf0f7931be7b44ce
Prince Raj <princeraj10d429bdps@gmail.com>
2024-10-26 22:41:32 +0530

    abc

---
+++ src/main/java/com/votingsystem/model/User.java
@@ -0,0 +1,34 @@
+package com.votingsystem.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String email;
+    private String password;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    // Getters and Setters
+    // ...
+}
